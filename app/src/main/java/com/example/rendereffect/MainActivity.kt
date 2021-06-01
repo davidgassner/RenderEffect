dda2e1ddc3a870367f659a1f4ee8b04c7326acdc
David Gassner <david.gassner@gmail.com>
2021-06-01 14:57:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/rendereffect/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.rendereffect
+
+import android.graphics.LightingColorFilter
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rendereffect.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.imageView.setRenderEffect(
+//            RenderEffect.createBlurEffect(
+//                30f,
+//                30f,
+//                Shader.TileMode.REPEAT
+//            )
+//        )
+
+//        binding.imageView.setRenderEffect(
+//            RenderEffect.createColorFilterEffect(
+//                LightingColorFilter(0xFF0000, 0x000000)
+//            )
+//        )
+    }
+}
